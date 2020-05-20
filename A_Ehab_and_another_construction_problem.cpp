bf25eb372d0d8e9383c795f172487ee8c08dc107
standmarsh <agrawalshagun07@gmail.com>
2020-05-20 15:47:55 +0530

    yes

---
+++ A_Ehab_and_another_construction_problem.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define ull unsigned long long int
+#define ll long long int
+#define P pair
+#define PLL pair<long long, long long>
+#define PII pair<int, int>
+#define PUU pair<unsigned long long int, unsigned long long int>
+#define L list
+#define V vector
+#define D deque
+#define ST set
+#define MS multiset
+#define M map
+#define UM unordered_map
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define MM multimap
+#define F first
+#define S second
+#define IT iterator
+#define RIT reverse_iterator
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie();                        \
+    cout.tie();
+#define FILE_READ_IN freopen("input.txt", "r", stdin);
+#define FILE_READ_OUT freopen("output.txt", "w", stdout);
+using namespace std;
+const ll maxn = 1e5;
+const ll inf = 1e9;
+const double pi = acos(-1);
+
+void solve()
+{
+    int x, a, b;
+    cin >> x;
+    int flag = 0;
+
+    for (a = 1; a <= x; a++)
+    {
+        for (b = 1; b <= x; b++)
+        {
+            if (flag == 0)
+            {
+                if (a % b == 0)
+                {
+                    if (a * b > x && a / b < x)
+                    {
+                        cout << a << " " << b;
+                        flag = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    if (flag == 0)
+    {
+        cout << "-1";
+    }
+}
+
+int main()
+{
+    int t = 1;
+    //cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

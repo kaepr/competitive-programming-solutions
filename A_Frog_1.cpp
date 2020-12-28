9949f14849c5c0b74b161b84c431b3f49092a870
Shagun Agrawal <agrawalshagun07@gmail.com>
2020-12-28 19:28:40 +0530

    finally atcoder dp contest start

---
+++ A_Frog_1.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+
+const int MOD = 1e9 + 7;
+
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+
+#define TRACE
+#ifdef TRACE
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char *name, Arg1 &&arg1)
+{
+    cout << name << " : " << arg1 << endl;
+    //use cerr if u want to display at the bottom
+}
+template <typename Arg1, typename... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&... args)
+{
+    const char *comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << " : " << arg1 << " | ";
+    __f(comma + 1, args...);
+}
+#else
+#define trace(...)
+#endif
+
+int powmod(int x, int y, int mod)
+{
+    int res = 1;
+    x %= mod;
+    if (x == 0)
+        return 0;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = (res * x) % mod;
+        y = y >> 1;
+        x = (x * x) % mod;
+    }
+    return res;
+}
+
+ll a[100005];
+ll dp[100005];
+
+ll ans(ll ptr)
+{
+    // cout << "Currently at : ";
+    // trace(ptr, a[ptr]);
+    if (ptr < 1)
+    {
+        return INT_MAX;
+    }
+
+    if (ptr == 1)
+    {
+        // dp[0] = 0;
+        return 0;
+    }
+
+    if (dp[ptr] != -1)
+    {
+        return dp[ptr];
+    }
+
+    dp[ptr] = min(ans(ptr - 1) + abs(a[ptr] - a[ptr - 1]), ans(ptr - 2) + abs(a[ptr] - a[ptr - 2]));
+
+    return dp[ptr];
+}
+
+void solve()
+{
+    memset(dp, -1, sizeof(dp));
+    dp[0] = 0;
+
+    ll n;
+    cin >> n;
+
+    for (ll i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+    cout << ans(n) << "\n";
+}
+
+int main()
+{
+    // freopen("filename_input.txt","r",stdin);
+    // freopen("filename_output.txt","w",stdout);
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    // cin >> t;
+    while (t > 0)
+    {
+        solve();
+        t--;
+    }
+
+    return 0;
+}

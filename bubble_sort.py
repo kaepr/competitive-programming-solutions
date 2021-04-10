46ec203a27142f526bc4b3a986a6b69d45491b25
Shagun Agrawal <agrawalshagun07@gmail.com>
2021-04-10 18:18:28 +0530

    deleting old

--- bubble_sort.py
+++
@@ -1,9 +0,0 @@
-times = [66, 57, 54, 53, 64, 52, 59]
-n = len(times)
-print(n)
-for i in range(n):
-    for j in range(0, n-i-1):
-        if times[j] > times[j+1]:
-            times[j], times[j+1] = times[j+1], times[j]
-
-print(times)
--- gcd.py
+++
@@ -1,8 +0,0 @@
-def gcd(a, b):
-    if a == 0:
-        return b
-    return gcd(b%a, a)
-
-a = 10234324324
-b = 151312321
-print(gcd(a,b))

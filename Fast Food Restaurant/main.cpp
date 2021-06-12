0a54b14ad96b79ccd0d5eb4f3e3b4d101ea864d3
Shagun Agrawal <agrawalshagun07@gmail.com>
2021-06-12 22:05:32 +0530

    testing

--- Fast Food Restaurant/main.cpp
+++ Fast Food Restaurant/main.cpp
@@ -1,8 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
+
+void print(vector<int> v){
+    for(auto x : v){
+        cout<<x<<" ";
+    }
+    cout<<"\n";
+}
 
 int main(){
-    cout<<"Hello World"<<  "\n";
+
+    vector<int> v = {6,5,3,2,1};
+
+    int x;
+    cout<<"Enter variable x = "<<"\n";
+    cin>>x;
+
+    cout<<"You entered x = "<<x<<"\n";
+
+
+    print(v);
+
+    sort(v.begin(), v.end());
+
+    print(v);
+
+    sort(v.begin(), v.end(), [](int x, int y){
+        return x>y;
+    });
+
+    print(v);
+    
+
     return 0;
 }

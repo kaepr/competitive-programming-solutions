908f86dc6cc45cf07f93412099e9b110ca4b6cb5
shagun agrawal <agrawalshagun07@gmail.com>
2022-09-03 00:29:05 +0530

    prblms

--- hashing/hashing.cpp
+++ hashing/hashing.cpp
@@ -1,134 +1,38 @@
-#include <bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+	vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
 
-using ll = long long;
-using ull = unsigned long long;
+		auto calc_dist = [&](const vector<int> &point) {
+			return point[0] * point[0] + point[1] * point[1];
+		};
 
-const ll MOD = 1e9 + 7;
+		auto comp = [&](const vector<int> &v1, const vector<int> &v2) {
+			return calc_dist(v1) < calc_dist(v2); // MAX HEAP
+		};
 
-/*
+		priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> pq(comp);
 
-Key components of hashing
---> Hash Function
---> Hash Table
---> Collision Handling Scheme
-
-Operations supported
---> Insert
---> Search
---> Delete
-
-Good Hash function properties
---> Reduce chance of collision - distribute keys uniformly over the table
---> should be fast to compute
-
-Table Size should be prime number
-
-Example: Hash function for strings
-	Take weighted sum
-	use all N characters of string as N digit base K number
-	Recommended to use K to be a prime number > 26
-
-//////////////////////////////////////////////////////////////////////////////
-
-Separate Chaining
-Open hashing
-Use linked lists to store extra nodes if collision happens
-
-*/
-
-////////////////////////////////////////////////////////////////////////////////
-// Counting total number of anagrams in a string
-
-
-// generate all sustrings
-// get ther hash value
-// add the hash value to a frequency map
-// count the total number of pairs possible
-
-vector<int> getHashValue(string s, int i, int j) {
-	vector<int> freq(26, 0);
-	for (int k = i; k <= j; k++) {
-		freq[s[k] - 'a']++;
-	}
-
-	return freq;
-}
-
-int total_anagram_substrings(string s) {
-	map<vector<int>, int> mp;
-
-	for (int i = 0; i < s.length(); i++) {
-		for (int j = i; j < s.length(); j++) {
-			vector<int> hashValue = getHashValue(s, i, j);
-			mp[hashValue]++;
+		for (int i = 0; i < k; i++) {
+			pq.push(points[i]);
 		}
-	}
 
-	int tot = 0;
-	for (auto x : mp) {
-		auto snd = x.second;
-		tot += (snd) * (snd - 1) / 2;
-	}
+		for (int i = k; i < points.size(); i++) {
+			auto point = pq.top();
+			int max_dist = calc_dist(point);
+			int cur_dist = calc_dist(points[i]);
 
-	return tot;
-}
-
-
-// least partitions required, similar to leetcode word break
-
-int min_bars_helper(string s, vector<string> &words, int idx, unordered_set<string> &st) {
-	if (idx == s.length()) {
-		return 0;
-	}
-
-	int ans = INT_MAX;
-
-	string current_string = "";
-
-	for (int j = idx; j < s.length(); j++) {
-		current_string += s[j];
-
-		if (st.find(current_string) != st.end()) {
-			int rem = min_bars_helper(s, words, j + 1, st);
-			if (rem != -1) {
-				ans = min(ans, 1 + rem);
+			if (cur_dist < max_dist) {
+				pq.pop();
+				pq.push(points[i]);
 			}
 		}
+
+		vector<vector<int>> ans;
+		while (!pq.empty()) {
+			ans.push_back(pq.top());
+			pq.pop();
+		}
+
+		return ans;
 	}
-
-	if (ans == INT_MAX) {
-		return -1;
-	}
-
-	return ans;
-}
-
-int min_bars(vector<string> &words, string s) {
-	unordered_set<string> st;
-	for (string word : words) {
-		st.insert(word);
-	}
-
-	int output = min_bars_helper(s, words, 0, st);
-	return output - 1;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-int main() {
-
-
-	return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
+};
--- recursion/recursion.cpp
+++ recursion/recursion.cpp
@@ -20,34 +20,126 @@ int countSubsetsEqualToX(vector<int> &arr, int n, int i, int X) {
 }
 
 
+// string s = "abcd";
+// vector<string> v;
 
-int main() {
+// for (int idx = 0; idx < s.length(); idx ++) {
+// 	string tmp = "";
+// 	for (int j = idx; j < s.length(); j++) {
+// 		tmp += s[j];
+// 		v.push_back(tmp);
+// 	}
+// }
 
-	string s = "abcd";
-	vector<string> v;
+// for (auto x : v) {
+// 	cout << x << "\n";
+// }
 
-	for (int idx = 0; idx < s.length(); idx ++) {
-		string tmp = "";
-		for (int j = idx; j < s.length(); j++) {
-			tmp += s[j];
-			v.push_back(tmp);
+// int sol() {
+
+
+// 	int curr = 0;
+// 	int elems = 0;
+// 	int ptr = 0;
+
+// 	int n = v.size();
+
+// 	int ans = curr;
+
+// deque<int> q;
+// while (ptr < n) {
+// 	// if it increases current sum
+// 	if (curr + v[ptr] >= 0) {
+// 		// if window is less than k
+// 		if (q.size() < k) {
+// 			// take it
+// 			q.push_back(v[ptr]);
+// 			curr += v[ptr];
+// 		} else {
+// 			// remove the last one
+// 			int to_remove = q.front();
+// 			q.pop_front();
+// 			curr -= to_remove;
+// 			curr += v[ptr];
+// 		}
+
+// 		ans = max(ans, curr);
+// 		ptr++;
+// 	} else {
+
+// 		}
+
+// 		return ans;
+// 	}
+// }
+
+
+long max_subarray_of_k(vector<int> &arr, int k, int n) {
+
+	long res = 0;
+	for (int i = 0; i < k; i++)
+		res += arr[i];
+
+	long curr_sum = res;
+	for (int i = k; i < n; i++) {
+		curr_sum += arr[i] - arr[i - k];
+		res = max(res, curr_sum);
+	}
+
+	return res;
+}
+
+long main2(vector<int> &arr, int n, int k) {
+	long ans = 0;
+
+	for (int i = 1; i <= k; i++) {
+		long cur = max_subarray_of_k(arr, i, n);
+		ans = max(ans, cur);
+	}
+
+	return ans;
+}
+
+
+long mainSol(vector<int> arr, int n, int k) {
+	vector<long> neg_sum(n, 0);
+
+	long curr = 0;
+
+	reverse(arr.begin(), arr.end());
+
+	for (int i = 0; i < n; i++) {
+		int idx = i;
+		int val = arr[i];
+		neg_sum[idx] = curr;
+		curr += val;
+
+		if (curr > 0) {
+			curr = 0;
 		}
 	}
 
-	for (auto x : v) {
-		cout << x << "\n";
-	}
+	reverse(neg_sum.begin(), neg_sum.end());
 
+	int i = 0, j = 0;
+	long ans = 0;
+	long cur = 0;
+
+	while (j < n) {
+		cur += arr[j];
+		if (cur + neg_sum[j] <= k) {
+
+		}
+	}
 
 	return 0;
 }
 
-
-
-
-
-
-
-
-
-
+int main() {
+	vector<int> v = {5, -7, 8, -6, 4, 1, -9, 5};
+	int k = 2;
+	int n = v.size();
+	mainSol(v, n, k);
+	cout << main2(v, v.size(), k) << "\n";
+	return 0;
+}

8f3dcf3a47579d47944961bef07a1038395dbd4b
Shagun Agrawal <agrawalshagun07@gmail.com>
2023-02-12 15:49:19 +0530

    add count inverse

--- solve/main.cpp
+++ solve/main.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,130 +7,51 @@ using vi = vector<int>;
 using vll = vector<long long>;
 
 ll powmod(ll x, ll y, ll mod) {
-  ll res = 1;
-  x %= mod;
-  if (x == 0)
-    return 0;
-  while (y > 0) {
-    if (y & 1)
-      res = (res * x) % mod;
-    y = y >> 1;
-    x = (x * x) % mod;
-  }
-  return res;
+    ll res = 1;
+    x %= mod;
+    if (x == 0)
+        return 0;
+    while (y > 0) {
+        if (y & 1)
+            res = (res * x) % mod;
+        y = y >> 1;
+        x = (x * x) % mod;
+    }
+    return res;
 }
 
 const ll MOD = 1e9 + 7;
 
-ll sum_digit(ll n) {
-  ll sum = 0;
-
-  while (n) {
-    sum += n % 10;
-    n /= 10;
-  }
-
-  return sum;
-}
-
-const ll N = 2e5 + 10;
-ll node[4 * N];
-
-void update(ll v, ll tl, ll tr, ll id, ll val) {
-  if (tl == id && tr == id) {
-    node[v] = val;
-    return;
-  }
-
-  if (id > tr || id < tl) {
-    return;
-  }
-
-  ll tm = tl + (tr - tl) / 2;
-  update(2 * v, tl, tm, id, val);
-  update(2 * v + 1, tm + 1, tr, id, val);
-
-  node[v] = node[2 * v + 1] + node[2 * v];
-}
-
-ll query(ll v, ll tl, ll tr, ll l, ll r) {
-  if (tl > r || tr < l) {
-    return 0;
-  }
-
-  if (l <= tl && tr <= r) {
-    // l ... tl ... tr ... r
-    //  cout << "HAPPENS\n";
-    return node[v];
-  }
-  ll tm = tl + (tr - tl) / 2;
-  ll ans = 0;
-  ans += query(2 * v, tl, tm, l, r);
-  ans += query(2 * v + 1, tm + 1, tr, l, r);
-  return ans;
-}
-
 void solve() {
-  ll n, q;
-  cin >> n >> q;
-  vll a(n);
-  for (ll i = 0; i < n; i++) {
-    cin >> a[i];
-  }
+    ll n;
+    long double m;
 
-  for (ll i = 0; i < n; i++) {
-    int val = 0;
-    if (a[i] > 9) {
-      val = 1;
-    }
-    update(1, 0, n - 1, i, (ll) val);
-  }
+    cin >> n >> m;
 
-  while (q) {
-    int choice;
-    cin >> choice;
+    long double Y = floor(m);
+    long double Z = m - Y;
 
-    if (choice == 1) {
-      ll l, r;
-      cin >> l >> r;
-      l--;
-      r--;
-
-      // after some number of operations, they will all become single digit
-
-      ll sum = query(1, 0, n - 1, l, r);
-      if (sum == 0) {
-        // nothing, no need to calculate again
-      } else {
-        for (ll i = l; i <= r; i++) {
-          a[i] = sum_digit(a[i]);
-          if (a[i] <= 9) {
-            update(1, 0, n - 1, i, 0);
-          }
+    while (true) {
+        long double dec = Z - floor(Z);
+        cout << Z << "\n";
+        if (dec <= 0.0) {
+            break;
         }
-      }
-    } else {
-      ll x;
-      cin >> x;
-      x--;
-      cout << a[x] << "\n";
+        Z *= 10;
     }
 
-    q--;
-  }
+    cout << Z << "\n";
 }
 
-int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(NULL);
-  cout.tie(NULL);
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  int t = 1;
-  cin >> t;
-  while (t > 0) {
-    solve();
-    t--;
-  }
-
-  return 0;
+    int t = 1;
+    cin >> t;
+    while (t > 0) {
+        solve();
+        t--;
+    }
 }
--- solve/main2.cpp
+++ solve/main2.cpp
@@ -1,108 +1,14 @@
 #include <bits/stdc++.h>
-#include <utility>
 using namespace std;
 
 using ll = long long;
 using ull = unsigned long long;
-using vi = vector<int>;
-using vll = vector<long long>;
-
-ll powmod(ll x, ll y, ll mod) {
-    ll res = 1;
-    x %= mod;
-    if (x == 0)
-        return 0;
-    while (y > 0) {
-        if (y & 1)
-            res = (res * x) % mod;
-        y = y >> 1;
-        x = (x * x) % mod;
-    }
-    return res;
-}
 
 const ll MOD = 1e9 + 7;
 
-struct Cost {
-    ll left;
-    ll right;
-    ll idx;
-};
+void solve() {}
 
-void solve() {
-    ll n, c;
-    cin >> n >> c;
-    vll a(n);
-
-    for (ll i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-
-    vll cost(n);
-    ll mn_cost = INT_MAX;
-    ll mn_idx = -1;
-
-    for (ll i = 0; i < n; i++) {
-        cost[i] = i + 1 + a[i];
-        if (cost[i] < mn_cost) {
-            mn_cost = cost[i];
-            mn_idx = i;
-        }
-    }
-
-    if (mn_cost > c) {
-        // cannot even take one element
-        cout << 0 << "\n";
-    }
-
-    ll ans = 1;
-    c -= mn_cost;
-
-    vector<pair<ll, ll>> v(n - 1);
-    ll idx = 0;
-    for (ll i = 0; i < n; i++) {
-        if (i != mn_idx) {
-            ll cost_left = i + 1 + a[i];
-            ll cost_right = (n - i) + a[i];
-            v[idx] = make_pair(cost_left, cost_right);
-            idx++;
-        }
-    }
-
-    sort(v.begin(), v.end(), [](auto c1, auto c2) {
-        vector<pair<ll, ll>> tmp;
-        tmp.push_back(make_pair(c1.first, 1));
-        tmp.push_back(make_pair(c1.second, 1));
-        tmp.push_back(make_pair(c2.first, 2));
-        tmp.push_back(make_pair(c2.second, 2));
-        sort(tmp.begin(), tmp.end());
-
-        if (tmp[0].second == 1) {
-            return true;
-        }
-        return false;
-    });
-    //
-    // for (auto x : v) {
-    //     cout << min(x.first, x.second) << " ";
-    // }
-    // cout << "\n";
-
-    ll used = 0;
-    for (ll i = 0; i < n - 1; i++) {
-        used += min(v[i].first, v[i].second);
-
-        if (used > c) {
-            break;
-        } else {
-            ans++;
-        }
-    }
-
-    cout << ans << "\n";
-}
-
-int main() {
+signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
@@ -113,6 +19,4 @@ int main() {
         solve();
         t--;
     }
-
-    return 0;
 }

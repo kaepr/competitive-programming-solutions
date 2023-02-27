5ce1c4c2fe63d54cdbeda4459f2103fb047d231d
Shagun Agrawal <agrawalshagun07@gmail.com>
2023-02-27 22:29:22 +0530

    readme

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## Contains boilerplate codes/solutions.
 
+Contains code / solutions or other boilerplate for competitive programming and dsa.
 
 ```bash
 ulimit -s unlimited # Setting stack in linux g++
--- solve/main.cpp
+++ solve/main.cpp
@@ -1,12 +1,10 @@
 #include <bits/stdc++.h>
-#include <deque>
-#include <queue>
-#include <utility>
+
 using namespace std;
 
 using ll = long long;
 using ull = unsigned long long;
-using vi = vector<int>;
+using vi = vector<ll>;
 using vll = vector<long long>;
 
 ll powmod(ll x, ll y, ll mod) {
@@ -25,43 +23,14 @@ ll powmod(ll x, ll y, ll mod) {
 
 const ll MOD = 1e9 + 7;
 
-void solve() {
-    int q;
-    cin >> q;
-
-    set<ll> st;
-    set<ll> snd; // does not contain entries inside st
-
-    while (q) {
-
-        ll choice, x;
-        cin >> choice >> x;
-
-        if (choice == (ll)1) {
-            st.insert(x);
-            snd.insert(x + 1);
-            if (snd.find(x) != snd.end()) {
-                snd.erase(x);
-            }
-        } else if (choice == (ll)2) {
-            auto it = snd.lower_bound(x + 1);
-            if (it == snd.end()) {
-                cout << x + 1 << "\n";
-            } else {
-                cout << *it << "\n";
-            }
-        }
-
-        q--;
-    }
-}
+void solve() {}
 
 signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int t = 1;
+    ll t = 1;
     cin >> t;
     while (t > 0) {
         solve();

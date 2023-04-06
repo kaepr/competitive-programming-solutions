c90a048348bb2afb0cf321fda780df3f09e76ea1
Shagun Agrawal <agrawalshagun07@gmail.com>
2023-04-06 18:10:17 +0530

    add more problems

--- solve/main.cpp
+++ solve/main.cpp
@@ -24,46 +24,8 @@ ll powmod(ll x, ll y, ll mod) {
 }
 
 const ll MOD = 1e9 + 7;
-const ll INF = 1e18;
-
-ll N, W;
-ll dp[105][100005];
-ll values[105];
-ll wts[105];
-
-ll rec(ll idx, ll cur) {
-    if (cur < 0) {
-        return -INF;
-    }
-
-    if (idx == N) {
-        return 0;
-    }
-
-    if (dp[idx][cur] != -1) {
-        return dp[idx][cur];
-    }
-
-    // don't take current item
-    ll ans = rec(idx + 1, cur);
-
-    // take current item and move
-    ans = max(ans, values[idx] + rec(idx + 1, cur - wts[idx]));
-
-    dp[idx][cur] = ans;
-    return ans;
-}
 
 void solve() {
-    cin >> N >> W;
-
-    memset(dp, -1, sizeof(dp));
-
-    for (ll i = 0; i < N; i++) {
-        cin >> wts[i] >> values[i];
-    }
-
-    cout << rec(0, W) << "\n";
 }
 
 signed main() {
@@ -72,9 +34,10 @@ signed main() {
     cout.tie(NULL);
 
     int t = 1;
-    // cin >> t;
+    cin >> t;
     while (t > 0) {
         solve();
         t--;
     }
 }
+
